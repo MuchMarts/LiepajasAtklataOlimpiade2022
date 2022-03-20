58a500119185bd1c65455f1a84c5b0052e8e796a
DrgnInventor <98103825+MuchMarts@users.noreply.github.com>
2022-03-20 19:25:36 +0200

    Refactoring of code

---
+++ map.java
@@ -0,0 +1,14 @@
+public class map {
+
+    static char[][] map;
+
+    public map(int x, int y) {
+        initMap(x, y);
+    }
+    
+    public static void initMap(int x, int y) {
+        map = new char[x][y];
+    }
+
+    
+}
