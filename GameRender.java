59727642e143f51f526d5635b5289e12c8b9fa8c
gdarta <dartagobleja@gmail.com>
2022-03-23 19:05:50 +0200

    added letter and number identificators

--- GameRender.java
+++ GameRender.java
@@ -1,10 +1,21 @@
-public class GameRender {
+public class GameRender { 
 
     public static void renderMap(char[][] map){
-        String[] line = new String[map.length];
-        for(int y = 0; y < map.length; y++){
-            for(int x = 0; x < map.length; x++){
-               System.out.print(map[x][y] + " ");
+    //prints out the map along with letter and number identificators
+        for(int y = 0; y < GameTuning.h; y++){
+            if(y<10 && y!=0){
+            System.out.print(" " + y + " ");
+            }else if(y==0){
+                System.out.print("   ");
+            }else{
+                System.out.print(y + " ");
+            }
+            for(int x = 0; x < GameTuning.w; x++){
+                if(y==0){
+                    System.out.print((char)(x + 17 + '0') + " ");
+                }else{
+                    System.out.print(map[x][y] + " ");
+                }
             }
             System.out.println();
         }
--- GameTuning.java
+++ GameTuning.java
@@ -1,6 +1,6 @@
 public class GameTuning {
-    public static int w = 10;   // World width (x)
-    public static int h = 10;   // World height (y)
+    public static int w = 20;   // World width (x)
+    public static int h = 11;   // World height (y)
     public static int visionRad = 1;    // Player vision radius
     public static int pxy = 2 * visionRad + 1;
 }
