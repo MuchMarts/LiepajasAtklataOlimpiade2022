dfdaba21342b6304a5f326ffa9c209b0ed45a064
gdarta <dartagobleja@gmail.com>
2022-03-26 14:39:52 +0200

    wtf

--- PlayerInfo.java
+++ PlayerInfo.java
@@ -1,3 +1,31 @@
 public class PlayerInfo {
+<<<<<<< Updated upstream
     
+=======
+    public String name;
+    public String playedTime;
+    public Boolean batteryDead;
+    public Double kilometers;
+
+    public PlayerInfo(String name){
+        this.name = name;
+    }
+
+    public void kilometersDriven(int ox, int oy, int nx, int ny){
+        //calculates the kilometeres driven between two coordinates
+        int distance = 224; //the distance(in km) between Riga and Liepaja
+        double border = distance/Math.sqrt(2);
+        double step_right = border/GameTuning.w; //the value of one square's edge(in km)
+        double step_diag = step_right/Math.sqrt(2); //the value of one square's diagonal(in km)
+        if(ox == nx){
+            kilometers = kilometers + (step_right * Math.abs(nx-ox));
+            System.out.println("Tu esi nobraucis " + kilometers + "km");
+        }else if(oy == ny){
+            kilometers = kilometers + (step_right * Math.abs(ny-oy));
+            System.out.println("Tu esi nobraucis " + kilometers + "km");
+        }else{
+            System.out.println("Tu esi nobraucis pa pieskari es nemaku aprekinat brr...");
+        }
+    }
+>>>>>>> Stashed changes
 }
