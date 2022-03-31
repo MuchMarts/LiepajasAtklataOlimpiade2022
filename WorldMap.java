70d37d5bd3d65f4748620f385842cc1d40ddf0c8
DrgnInventor <98103825+MuchMarts@users.noreply.github.com>
2022-03-31 17:56:09 +0300

    I mean it draws paths maybe ugly buy ugh

--- WorldMap.java
+++ WorldMap.java
@@ -1,4 +1,5 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WorldMap {
     
@@ -10,6 +11,9 @@ public class WorldMap {
     public int width;
     public int height;
     public int[] playerLastCordinates;
+    CheckpointParser ch;
+    Translate translate;
+
 
     public WorldMap(int w, int h, CheckpointParser ch){
         this.width = w;
@@ -17,46 +21,84 @@ public class WorldMap {
         this.playerLastCordinates = new int[2];
         this.playerLastCordinates[0] = GameSettings.start_x;
         this.playerLastCordinates[1] = GameSettings.start_y;
+        this.ch = new CheckpointParser();
+        this.translate = new Translate();
 
+        genPaths();
         initMap();
 
+    }
+
+    private void genPaths(){
+        
+        this.paths = new int[this.width][this.height]; 
+
+        int[][] temp_paths = new int[this.width][this.height]; 
+        
+        for(int ty = 0; ty < this.height; ty++){
+            for(int tx = 0; tx < this.width; tx++){
+                temp_paths[ty][tx] = 1;
+            }   
+        }
+
+        String roadcoor;
+        
+        for(int y = 0; y < this.height; y++){
+            for(int x = 0; x < this.width; x++){
+
+                roadcoor = ch.getLetter(x) + Integer.toString(y + 1);
+                if(ch.isCheckPoint(roadcoor)){
+                    int[] point = {y, x};
+
+                    ArrayList<String> allNodes = new ArrayList<>(Arrays.asList(ch.checkpointsInAMap().get(roadcoor).get("nodes").split(", ")));
+                    
+                    for(String end : allNodes){
+                        if(end == ""){break;}
+
+                        int xCor = Integer.parseInt(end.substring(1));
+                        int yCor = translate.getInteger(end.substring(0,1).toUpperCase());
+
+                        int[] cord = {yCor - 1, xCor};
+                        int[][] road = shortestPathBetweenPoints.findShortestPath(temp_paths, point, cord);
+
+                        setRoad(road);
+                    }
+                } 
+            }
+        }   
+    }
+
+    private void setRoad(int[][] roads){
+        for(int[] xy : roads){
+            this.paths[xy[1]][xy[0]] = 1;
+        }
     }
 
     private void initMap(){
         this.gameMap = new char[this.width][this.height];
         //TODO: Read map info from somewhere
-        CheckpointParser ch = new CheckpointParser();
         //Intializes map data (RN TEMP FILLED WITH -)
+
+        //Fill map with path data
+        for(int y = 0; y < this.height; y++){
+            for(int x = 0; x < this.width; x++){
+                if(this.paths[y][x] == 1){
+                    this.gameMap[x][y] = '=';
+                } else {
+                    this.gameMap[x][y] = '-';
+                }
+                }
+            }
 
         for(int y = 0; y < this.height; y++){
             for(int x = 0; x < this.width; x++){
-                this.gameMap[x][y] = '-';
                 String coor = ch.getLetter(x) + Integer.toString(y + 1);
                 if(ch.checkpointsInAMap().containsKey(coor)){
                     this.gameMap[x][y] = '@';
-                }else if(ch.chargeStationsInAMap().containsKey(coor)){
+                } else if(ch.chargeStationsInAMap().containsKey(coor)){
                     this.gameMap[x][y] = '$';
-                }
+                 }
             }
-        }
-
-        int[][] tempPath = {
-            {0,0},
-            {0,1},
-            {0,2},
-            {0,3},
-            {0,4},
-            {1,5},
-            {2,5},
-            {3,5},
-            {3,4},
-            {3,3},
-            {2,3}
-
-        };
-
-        for(int[] cords : tempPath){
-            this.gameMap[cords[0]][cords[1]] = '=';
         }
 
         this.playerLocation = new char[this.width][this.height]; 
@@ -68,18 +110,6 @@ public class WorldMap {
                 }
                 }
             }
-        this.paths = new int[this.width][this.height]; 
-        //Init Path map
-        for(int y = 0; y < this.height; y++){
-            for(int x = 0; x < this.width; x++){
-                if(gameMap[x][y] == '=' || gameMap[x][y] == '@' || gameMap[x][y] == '$'){
-                    paths[y][x] = 1;
-                } else {
-                    paths[y][x] = 0;
-                }
-            }
-        }
-
     }
 
     //Updates player location
